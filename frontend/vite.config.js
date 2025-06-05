5b598337334b83d4a9c3b9a927a0abb482e5d975
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-06-05 16:19:16 +0400

    Refactor code structure for improved readability and maintainability

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
+            extType = 'fonts';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
+  publicDir: 'public',
 })
